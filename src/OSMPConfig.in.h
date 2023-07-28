ad9f19274722b04df7ee67a49fe72170a1718283
Clemens Linnhoff <clemens.linnhoff@partner.bmw.de>
2023-07-28 11:52:59 +0200

    Update copyright and license information in headers
    The header comments in OSMPConfig.in.h were changed to update the copyright notice and licensing information. This was done to reflect the additional copyright by BMW AG in 2023, and to simplify the licensing information by including only the SPDX short identifier.
    
    Signed-off-by: ClemensLinnhoff <clemens.linnhoff@partner.bmw.de>

--- src/OSMPConfig.in.h
+++ src/OSMPConfig.in.h
@@ -1,12 +1,8 @@
-/*
- * PMSF FMU Framework for FMI 2.0 Co-Simulation FMUs
- *
- * (C) 2016 -- 2018 PMSF IT Consulting Pierre R. Mai
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+//
+// Copyright 2016 -- 2018 PMSF IT Consulting Pierre R. Mai
+// Copyright 2023 BMW AG
+// SPDX-License-Identifier: MPL-2.0
+//
 
 #cmakedefine PUBLIC_LOGGING
 #cmakedefine PRIVATE_LOG_PATH "@PRIVATE_LOG_PATH@"
